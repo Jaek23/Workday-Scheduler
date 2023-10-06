cf8f4a9ffa103e76b2ea4faa90eeff60be4cf285
jae kim <jaehkim23@gmail.com>
2023-10-06 00:36:42 -0500

    added current date/worked on hours if conditional

--- assets/script.js
+++ assets/script.js
@@ -14,10 +14,18 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var date = new Date();
+  var hour = date.getHours();
+  var currentHour = hour;
+  if(currentHour = hour){
+    $('textarea').attr('class', 'present');
+  }
+ 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs();
+  $('#currentDay').text(currentDate.format('dddd, MMMM D, YYYY'));
 });
